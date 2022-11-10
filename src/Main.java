14adb7e758136c0eaff5e9bea44be996e8294a93
kch4731 <kch4731@naver.com>
2022-11-10 22:57:32 +0900

    Add : 10953문제 삼항 연산자 사용하여 더 짧게 시도해 봄

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int n = Integer.parseInt(br.readLine());
-		for (int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), ",");
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			System.out.println(a + b);
+		String str = br.readLine();
+		for (int i = 0; i < str.length(); i++) {
+			changeROT13(str.charAt(i));
 		}
+	}
+
+	private static void changeROT13(char c) {
+		if (c >= 'a' && c <= 'z') {
+			c = changeROT13LowerCase(c);
+		} else if (c >= 'A' && c <= 'Z') {
+			c = changeROT13UpperCase(c);
+		}
+		System.out.print(c);
+	}
+
+	private static char changeROT13UpperCase(char c) { // 소문자인 경우
+		c += 13;
+		return (c > 'Z') ? (char) (c - 26) : c;
+	}
+
+	private static char changeROT13LowerCase(char c) { // 대문자인 경우
+		c += 13;
+		return (c > 'z') ? (char) (c - 26) : c;
 	}
 }
