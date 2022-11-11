67c2eeeb6fb3222823d01eb949f8382f4f89530d
VIRTUKCH <114549229+VIRTUKCH@users.noreply.github.com>
2022-11-11 11:28:06 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -7,10 +7,11 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		char[] charArray = br.readLine().toCharArray();
 
+		char temp = ' ';
 		for (int i = 0; i < charArray.length; i++) {
 			for (int j = 0; j < charArray.length - 1; j++) {
 				if (charArray[j] < charArray[j + 1]) {
-					char temp = charArray[j + 1];
+					temp = charArray[j + 1];
 					charArray[j + 1] = charArray[j];
 					charArray[j] = temp;
 				}
