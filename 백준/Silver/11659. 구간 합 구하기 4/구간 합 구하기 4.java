1c8cdaf70e130f5003a7a190f05dec2de2d7ab15
VIRTUKCH <114549229+VIRTUKCH@users.noreply.github.com>
2023-08-17 05:23:09 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1632 ms, Memory: 58436 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -6,21 +6,22 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), "+ ");
-		int suNo = Integer.parseInt(st.nextToken());
-		int quizNo = Integer.parseInt(st.nextToken());
 
-		long[] S = new long[suNo + 1]; // SumArray의 개수는 하나 더 만든다.
+		int numberOfData = Integer.parseInt(st.nextToken());
+		int numberOfQuiz = Integer.parseInt(st.nextToken());
 
-		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= suNo; i++) {
-			S[i] = S[i-1] + Integer.parseInt(st.nextToken()); // 5, 4, 3, 2, 1
+		int[] S = new int[numberOfData + 1];
+
+		st = new StringTokenizer(br.readLine(), "+ ");
+		for (int i = 1; i <= numberOfData; i++) {
+			S[i] = S[i-1] + Integer.parseInt(st.nextToken());
 		}
 
-		for (int i = 0; i < quizNo; i++) {
-			st = new StringTokenizer(br.readLine());
-			int p = Integer.parseInt(st.nextToken());
-			int q = Integer.parseInt(st.nextToken());
-			System.out.println(S[q] - S[p-1]);
+		for (int i = 0; i < numberOfQuiz; i++) {
+			st = new StringTokenizer(br.readLine(), "+ ");
+			int start = Integer.parseInt(st.nextToken());
+			int last = Integer.parseInt(st.nextToken());
+			System.out.println(S[last] - S[start - 1]);
 		}
 	}
 }
