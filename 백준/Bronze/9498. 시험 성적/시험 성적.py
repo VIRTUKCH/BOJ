2f701d59d83b71636120ced1b7a57f5d39aa3d68
Virtus_Chae <114549229+VIRTUKCH@users.noreply.github.com>
2023-10-23 13:26:48 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,15 @@
+score = int(input())
+score = int(score/10)
+
+if(score == 10):
+  print('A')
+elif(score == 9):
+  print('A')
+elif(score == 8):
+  print('B')
+elif(score == 7):
+  print('C')
+elif(score == 6):
+  print('D')
+else:
+  print('F')
