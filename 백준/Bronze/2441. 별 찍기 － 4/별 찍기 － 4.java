c75defb88edee51175faca5336052bcd4f587ca1
Virtus_Chae <114549229+VIRTUKCH@users.noreply.github.com>
2023-10-23 13:27:28 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 272 ms, Memory: 16712 KB -BaekjoonHub

--- 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -1,20 +1,17 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		for(int i=n; i>0; i--)
-		{
-			for(int j=0; j<n-i; j++)
-			{
+
+		for (int i = 0; i < n; i++) {
+			for (int k = 0; k < i; k++) {
 				System.out.print(" ");
 			}
-			
-			for(int k=i; k>0; k--)
-			{
+
+			for (int j = 0; j < n-i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
