7b7559510b310d426637481a40e860a7cbedaaea
Virtus_Chae <114549229+VIRTUKCH@users.noreply.github.com>
2023-10-28 17:51:37 +0900

    [Silver IV] Title: 기타줄, Time: 120 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.java
+++ 백준/Silver/1049. 기타줄/기타줄.java
@@ -17,32 +17,24 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int priceOfPackage = Integer.parseInt(st.nextToken());
             int priceOfOneLine = Integer.parseInt(st.nextToken());
-
+            
             if (priceOfCheapestOneLine > priceOfOneLine) {
                 priceOfCheapestOneLine = priceOfOneLine;
             }
-
+            
             if (priceOfCheapestPackage > priceOfPackage) {
                 priceOfCheapestPackage = priceOfPackage;
             }
         }
-
+        
         if (((int) (brokenString / 6) + 0.99) * priceOfCheapestPackage // 1. 패키지를 하나 더 사는 게 나으면, 패키지를 사는 게 낫다.
                 <= (int) (brokenString / 6) * priceOfCheapestPackage + ((int) brokenString % 6) * priceOfCheapestOneLine) {
             result = (int) ((brokenString / 6) + 0.99) * priceOfCheapestPackage;
-//            System.out.println(1);
         } else if (priceOfCheapestPackage <= 6 * priceOfCheapestOneLine) { // 2. 패키지 < 여섯 줄이면 패키지 + 낱개 줄을 사시고
             result = (int) ((priceOfCheapestPackage * (int) (brokenString / 6) + priceOfCheapestOneLine * (brokenString % 6)));
-//            System.out.println(2);
-//            System.out.println("priceOfCheapestPackage = " + priceOfCheapestPackage);
-//            System.out.println("priceOfCheapestOneLine = " + priceOfCheapestOneLine);
-//            System.out.println("brokenString = " + brokenString);
-        }
-        else { // 3. 아니면 패키지 > 여섯 줄이면 -> 그냥 낱개로 사요
+        } else { // 3. 아니면 패키지 > 여섯 줄이면 -> 그냥 낱개로 사요
                 result = (int) (priceOfCheapestOneLine * brokenString);
-//                System.out.println(3);
-            }
-
-            System.out.println(result);
         }
+        System.out.println(result);
     }
+}
