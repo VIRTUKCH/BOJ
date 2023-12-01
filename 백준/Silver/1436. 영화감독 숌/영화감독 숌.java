8b09c510362a45fbb71fac1b063089b6376a3c81
Virtus_Chae <114549229+VIRTUKCH@users.noreply.github.com>
2023-12-01 23:24:13 +0900

    [Silver V] Title: 영화감독 숌, Time: 300 ms, Memory: 85832 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -10,16 +10,17 @@ public class Main {
         int cnt=0;
 
         // 1. 666부터 무한대까지, 만약 연속된 666이 들어가는 수라면 리스트에 추가한다.
-        ArrayList<Integer> integerArrayList = new ArrayList<>();
+//        ArrayList<Integer> integerArrayList = new ArrayList<>();
         for (int i = 666; i < Integer.MAX_VALUE; i++) {
             String str = String.valueOf(i);
             if(str.contains("666")) {
-                integerArrayList.add(Integer.parseInt(str));
+//                integerArrayList.add(Integer.parseInt(str));
                 cnt++;
             }
 
             if(cnt == n) {
-                System.out.println(integerArrayList.get(n-1));
+//                System.out.println(integerArrayList.get(n-1));
+                System.out.println(i);
                 return;
             }
         }
