498d94bda6bd30cef16863f2f1a47c69d7faa573
Virtus_Chae <114549229+VIRTUKCH@users.noreply.github.com>
2023-12-08 11:08:49 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14404 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,17 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.math.BigInteger;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int L = Integer.parseInt(br.readLine());
         String str = br.readLine();
-        Long result = 0L;
-        for (int i = 0; i < n; i++) {
-            result += (long) ((long) (str.charAt(i) - 96) * Math.pow(31, i));
+
+        BigInteger hash = new BigInteger("0");
+        BigInteger r = new BigInteger("31");
+        BigInteger mod = new BigInteger("1234567891");
+
+        for (int i = 0; i < L; i++){
+            BigInteger ch;
+            ch = BigInteger.valueOf((str.charAt(i)-'a')+1);
+
+            hash = hash.add(r.pow(i).mod(mod).multiply(ch));
         }
-        System.out.println(result);
+        System.out.println(hash.mod(mod));
     }
 }
