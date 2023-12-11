1d09d45b372f7c16a9a64b0ae9f0f6c8dd5eabf3
Virtus_Chae <114549229+VIRTUKCH@users.noreply.github.com>
2023-12-11 21:56:53 +0900

    [Bronze V] Title: N 찍기, Time: 216 ms, Memory: 17864 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append(i).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
