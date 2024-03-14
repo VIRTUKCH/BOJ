2d64c69d57b0e955a1afbad546c8e489e540b58f
Virtus_Chae <114549229+VIRTUKCH@users.noreply.github.com>
2024-03-14 13:57:16 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1552 ms, Memory: 58352 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+class Point implements Comparable {
+    int x;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if (o instanceof Point) {
+            if (((Point) o).getX() < this.getX()) {
+                return 1;
+            } else if (((Point) o).getX() > this.getX()) {
+                return -1;
+            } else {
+                if (((Point) o).getY() < this.getY()) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        }
+        else {
+            return Integer.MAX_VALUE;
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Point[] points = new Point[N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            points[i] = new Point(x, y);
+        }
+        Arrays.sort(points);
+
+        for (Point p : points) {
+            System.out.println(p.getX() + " " + p.getY());
+        }
+    }
+}
