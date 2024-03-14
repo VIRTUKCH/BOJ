e646874dbd39654246018d75c3cc65b67afb0aa5
Virtus_Chae <114549229+VIRTUKCH@users.noreply.github.com>
2024-03-14 21:52:29 +0900

    [Silver V] Title: 단어 정렬, Time: 336 ms, Memory: 26016 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -16,12 +16,10 @@ public class Main {
         Arrays.sort(strings, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                if(o1.length() > o2.length()) {
-                    return 1;
-                } else if (o1.length() < o2.length()) {
-                    return -1;
-                } else {
+                if(o1.length() == o2.length()) {
                     return o1.compareTo(o2);
+                } else {
+                    return o1.length() - o2.length();
                 }
             }
         });
