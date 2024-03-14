c5e5d84d039a6e23f1541100dbbade53f42627d3
Virtus_Chae <114549229+VIRTUKCH@users.noreply.github.com>
2024-03-14 21:49:42 +0900

    [Silver V] Title: 단어 정렬, Time: 388 ms, Memory: 27208 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] strings = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            strings[i] = br.readLine();
+        }
+
+        Arrays.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() > o2.length()) {
+                    return 1;
+                } else if (o1.length() < o2.length()) {
+                    return -1;
+                } else {
+                    return o1.compareTo(o2);
+                }
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N-1; i++) {
+            if(!strings[i].equals(strings[i+1])) {
+                sb.append(strings[i] + "\n");
+            }
+        }
+        sb.append(strings[N-1]);
+        System.out.println(sb);
+    }
+}
