8074708e8eb1da7075e500651855d85cd981cb24
Virtus_Chae <114549229+VIRTUKCH@users.noreply.github.com>
2024-03-15 09:47:20 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 708 ms, Memory: 49940 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -2,42 +2,34 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
+
 class Point implements Comparable {
     int x;
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
     int y;
 
-    Point(int x, int y) {
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
     @Override
     public int compareTo(Object o) {
-        if (o instanceof Point) {
-            if (((Point) o).getY() < this.getY()) { // y가 작으면 뒤로
+        if(o instanceof Point) {
+            if(this.y > ((Point)o).y) {
                 return 1;
-            } else if (((Point) o).getY() > this.getY()) { // y가 크면 앞으로
+            } else if (this.y < ((Point)o).y) {
                 return -1;
             } else {
-                if (((Point) o).getX() < this.getX()) { // x가 작으면 뒤로
+                if(this.x > ((Point)o).x) {
                     return 1;
-                } else { // x가 크면 앞으로
+                } else if (this.x < ((Point)o).x) {
                     return -1;
+                } else {
+                    return Integer.MAX_VALUE;
                 }
             }
         }
-        else {
-            return Integer.MAX_VALUE;
-        }
+        return Integer.MAX_VALUE;
     }
 }
 
@@ -45,17 +37,26 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+
         Point[] points = new Point[N];
+
+        StringTokenizer st;
+
+        int x = 0;
+        int y = 0;
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
+            st = new StringTokenizer(br.readLine());
+            x = Integer.parseInt(st.nextToken());
+            y = Integer.parseInt(st.nextToken());
             points[i] = new Point(x, y);
         }
+
         Arrays.sort(points);
 
-        for (Point p : points) {
-            System.out.println(p.getX() + " " + p.getY());
+        StringBuilder sb = new StringBuilder();
+        for (Point p: points) {
+            sb.append(p.x).append(" ").append(p.y).append("\n");
         }
+        System.out.println(sb);
     }
 }
