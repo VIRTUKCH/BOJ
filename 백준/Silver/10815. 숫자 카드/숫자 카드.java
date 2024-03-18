9af05c8a224ccdacf4102ba1f39d6172175fd057
Virtus_Chae <114549229+VIRTUKCH@users.noreply.github.com>
2024-03-18 22:07:39 +0900

    [Silver V] Title: 숫자 카드, Time: 952 ms, Memory: 137820 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -5,12 +5,6 @@ import java.util.HashSet;
 import java.util.SimpleTimeZone;
 import java.util.StringTokenizer;
 
-/*
- * 1. 상근이 카드 집합에 박기
- * 2. 구해야 할 숫자 배열에 박기
- * 3. 배열 탐색 + 집합에 있으면 1 없으면 0 반환하기
- */
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -35,9 +29,9 @@ public class Main {
         // 3. 배열 탐색 + 집합에 있으면 1 없으면 0 반환하기
         for (int n : checkCardArrays) {
             if(integerHashSet.contains(n)) {
-                sb.append(1).append(" ");
+                sb.append("1 ");
             } else {
-                sb.append(0).append(" ");
+                sb.append("0 ");
             }
         }
         System.out.println(sb);
