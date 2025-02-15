d914ada41fd9e716aa7b06e29b08ffd1c7c56165
Kim Chae Ho <114549229+VIRTUKCH@users.noreply.github.com>
2025-02-15 15:20:27 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1392 ms, Memory: 64124 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,27 +1,48 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.StringTokenizer;
 
+import javax.imageio.metadata.IIOMetadataFormatImpl;
+
+// Run Shortcut => Shift + Command + Function + F11
 public class Main {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), "+ ");
-
-		int numberOfData = Integer.parseInt(st.nextToken());
-		int numberOfQuiz = Integer.parseInt(st.nextToken());
-
-		int[] S = new int[numberOfData + 1];
-
-		st = new StringTokenizer(br.readLine(), "+ ");
-		for (int i = 1; i <= numberOfData; i++) {
-			S[i] = S[i-1] + Integer.parseInt(st.nextToken());
+		
+		int N = 0, M = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		long[] arrayValue = new long[N];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < arrayValue.length; i++) {
+			arrayValue[i] = Long.parseLong(st.nextToken());
 		}
-
-		for (int i = 0; i < numberOfQuiz; i++) {
-			st = new StringTokenizer(br.readLine(), "+ ");
-			int start = Integer.parseInt(st.nextToken());
-			int last = Integer.parseInt(st.nextToken());
-			System.out.println(S[last] - S[start - 1]);
+		
+		long[] sumArrayValue = new long[N];
+		for (int i = 0; i < sumArrayValue.length; i++) {
+			if(i>0) {
+				sumArrayValue[i] = arrayValue[i] + sumArrayValue[i-1];
+			} else {
+				sumArrayValue[i] = arrayValue[i];
+			}
+		}
+		
+		int a = 0, b = 0;
+		// 1. 여러 번 받아
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			if(a == 1) {
+				System.out.println(sumArrayValue[b-1]);
+			} else {
+				System.out.println(sumArrayValue[b-1] - sumArrayValue[a-2]);
+			}
 		}
 	}
 }
