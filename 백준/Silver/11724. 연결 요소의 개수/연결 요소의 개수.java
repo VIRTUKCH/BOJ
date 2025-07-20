59c2010820b56fb7ff1d56db6a9930fad4590927
Kim Chae Ho <114549229+VIRTUKCH@users.noreply.github.com>
2025-07-20 17:21:41 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 624 ms, Memory: 141976 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,58 +1,56 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
+public class Main {
 
-        static ArrayList<Integer>[] A;  // 인접연결리스트
-        static boolean visited[];       // 방문했는가?
+	static List<List<Integer>> list;
+	static boolean[] visited;
 
-        public static void main(String[] args) throws IOException {
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-                int N = Integer.parseInt(st.nextToken());       // node
-                int M = Integer.parseInt(st.nextToken());       // edge
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
-                A = new ArrayList[N+1];
-                visited = new boolean[N+1];
+		list = new ArrayList<>();
 
-                for (int i = 1; i < N + 1; i++) {
-                        A[i] = new ArrayList<>();
-                }
+		// 여기서 답안지 한 번 확인했음 (Syntax)
+		for (int i = 0; i <= N; i++) {
+			list.add(new ArrayList<>());
+		}
 
-                // setting!
-                for (int i = 0; i < M; i++) {
-                        st = new StringTokenizer(br.readLine());
-                        int u = Integer.parseInt(st.nextToken());
-                        int v = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
 
-                        // add on ArrayList
-                        A[u].add(v);
-                        A[v].add(u);
-                }
+			list.get(u).add(v);
+			list.get(v).add(u);
+		}
 
-                // solve!
-                int count = 0;
-                for (int i = 1; i < N + 1; i++) {
-                        if (!visited[i]) {
-                                count++;
-                                DFS(i);
-                        }
-                }
-                System.out.println(count);
-        }
+		visited = new boolean[N+1];
 
-        static void DFS(int v) {
-                if (visited[v]) {
-                        return;
-                }
+		int count = 0;
+		for (int i = 1; i <= N; i++) {
+			if (!visited[i]) {
+				DFS(i);
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
 
-                visited[v] = true;
+	static void DFS(int vertexIdx) {
+		if (visited[vertexIdx]) {
+			return;
+		}
 
-                for (int i : A[v]) {
-                        if (visited[i] == false) {
-                                DFS(i);
-                        }
-                }
-        }
+		visited[vertexIdx] = true;
+		List<Integer> nextVertexList = list.get(vertexIdx);
+
+		for (int nextVertexIdx : nextVertexList) {
+			DFS(nextVertexIdx);
+		}
+	}
 }
