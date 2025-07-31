173f3de37a27ccad19e5702322092daada8645c7
Kim Chae Ho <114549229+VIRTUKCH@users.noreply.github.com>
2025-08-01 00:38:09 +0900

    [Gold V] Title: ABCDE, Time: 228 ms, Memory: 24184 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -3,72 +3,67 @@ import java.io.*;
 
 public class Main {
 
-        public static List<List<Integer>> list;
-        public static boolean[] visited;
-        public static boolean finishFlag;
+	public static List<List<Integer>> list;
+	public static boolean[] visited;
+	public static boolean flag;
 
-        public static void main(String[] args) throws IOException {
-                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-                int n = Integer.parseInt(st.nextToken());
-                int m = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 
-                // init list
-                list = new ArrayList<>();
-                for (int i = 0; i < n; i++) {
-                        list.add(new ArrayList<>());
-                }
+		// init list
+		list = new ArrayList<>();
 
-                // init boolean Array
-                visited = new boolean[n];
+		// init boolean array
+		visited = new boolean[n];
 
-                // init list 2 - 리스트에 원소 추가하기
-                for (int i = 0; i < m; i++) {
-                        st = new StringTokenizer(br.readLine());
-                        int a = Integer.parseInt(st.nextToken());
-                        int b = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < n; i++) {
+			list.add(new ArrayList<>());
+		}
 
-                        list.get(a).add(b);
-                        list.get(b).add(a);
-                }
+		// input
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 
-                // 이중 접점 리스트 점검 결과 이상 없음
+			list.get(a).add(b);
+			list.get(b).add(a);
+		}
 
-                // depth 가 4이면 1을 출력해야 하고
-                // 4 가 안 되는데 + 더 갈 곳도 없으면 return 해야 함
-                // 일단 탐색을 다 하는 게 맞음
+		// visit
+		for (int i = 0; i < n; i++) {
+			DFS(i, 0);
+			if (flag == true) {
+				System.out.println(1);
+				return;
+			}
+		}
+		System.out.println(0);
+	}
 
-                for (int i = 0; i < n; i++) {
-                        DFS(i, 0);
-                        if (finishFlag == true) {
-                                System.out.println(1);
-                                return;
-                        }
-                }
+	public static void DFS(int indexOfNode, int depth) {
+		if (visited[indexOfNode] == true) {
+			return;
+		}
 
-                System.out.println(0);
-                return;
-        }
+		if (depth == 4) {
+			flag = true;
+			return;
+		}
 
-        public static void DFS(int indexOfNode, int depth) {
-                if (visited[indexOfNode] == true) {
-                        return;
-                }
+		visited[indexOfNode] = true;
+		depth++;
 
-                if (depth == 4) {
-                        finishFlag = true;
-                        return;
-                }
+		// next node
+		List<Integer> nextNodeList = list.get(indexOfNode);
+		for (int nextNodeIndex : nextNodeList) {
+			DFS(nextNodeIndex, depth);
+		}
 
-                visited[indexOfNode] = true;
-                depth++;
-
-                List<Integer> nextNodeList = list.get(indexOfNode);
-                for (int nextNodeIndex : nextNodeList) {
-                        DFS(nextNodeIndex, depth);
-                }
-
-                visited[indexOfNode] = false;
-        }
+		visited[indexOfNode] = false;
+	}
 }
