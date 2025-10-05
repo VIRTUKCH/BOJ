199275487c0f49d0c3715f158331854cfd5f2bbb
Kim Chae Ho <114549229+VIRTUKCH@users.noreply.github.com>
2025-10-05 15:39:43 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 784 ms, Memory: 162552 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,103 +1,100 @@
+
 import java.util.*;
 import java.io.*;
 
 public class Main {
 
-	public static int[][] board;
-	public static boolean[][][] visited;
-	public static int[][][] dist;
-
-	public static Queue<int[]> queue;
-	public static int count;
-
 	public static int n;
 	public static int m;
+
+	public static int[][] map;
+	public static Queue<int[]> queue;
+	public static int[][][] dist;
 
 	public static int[] dx = new int[] {-1, 1, 0, 0};
 	public static int[] dy = new int[] {0, 0, -1, 1};
 
-	public static void main (String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 
+
 		// init variables
-		board = new int[n][m];
-		visited = new boolean[n][m][2];
-		dist = new int[n][m][2];
+		map = new int[n][m];
 		queue = new LinkedList<>();
-		count = 0;
+		dist = new int[n][m][2];
 
 		// get input
 		for (int i = 0; i < n; i++) {
 			String str = br.readLine();
 			for (int j = 0; j < m; j++) {
-				int c = str.charAt(j) - '0';
-				board[i][j] = c;
+				int value = str.charAt(j) - '0';
+				map[i][j] = value;
 			}
 		}
 
-		// print test
-		//for (int i = 0; i < n; i++) {
-		//	System.out.println(Arrays.toString(board[i]));
+		// map print test
+		//for (int[] array : map) {
+		//	System.out.println(Arrays.toString(array));
 		//}
-
-		BFS(0, 0);
-		int ans0 = dist[n-1][m-1][0];
-		int ans1 = dist[n-1][m-1][1];
-
-		int result;
-		if (ans0 == 0 && ans1 == 0) result = -1;
-		else if (ans0 == 0) result = ans1;
-		else if (ans1 == 0) result = ans0;
-		else result = Math.min(ans0, ans1);
-
+		
+		int result = BFS(0, 0, 0);
 		System.out.println(result);
-
 	}
 
-	public static void BFS(int x, int y) {
-		if (visited[x][y][0] == true) {
-			count = -1;
-			return;
-		}
-
-		visited[x][y][0] = true;
-		queue.add(new int[]{x, y, 0});
-		dist[x][y][0] = 1;
-
-		while (queue.isEmpty() == false) {
+	public static int BFS(int x, int y, int z) {
+		queue.add(new int[] {x, y, z});
+		dist[x][y][z] = 1;
+		while(queue.isEmpty() == false) {
 			int[] poll = queue.poll();
 			int px = poll[0];
 			int py = poll[1];
-			int broken = poll[2];
+			int pz = poll[2];
 
-			//System.out.println("px : " + px + ", py : " + py + ", dist : " + dist[px][py][broken]);
+			if (px == n-1 && py == m-1) {
+				return dist[px][py][pz];
+			}
 
 			for (int i = 0; i < 4; i++) {
 				int nx = px + dx[i];
 				int ny = py + dy[i];
+				int nz = pz;
 
-				if (nx < 0 || ny < 0 || nx >= n || ny >= m || visited[nx][ny][broken]) {
+				// can't go?
+				if (nx < 0 || ny < 0 || nx >= n || ny >= m) {
 					continue;
 				}
 
-				// 1. 벽 아니지롱
-				if (board[nx][ny] == 0 && visited[nx][ny][broken] == false) {
-					visited[nx][ny][broken] = true;
-					queue.add(new int[] {nx, ny, broken});
-					dist[nx][ny][broken] = dist[px][py][broken] + 1;
+				// visited?
+				if (dist[nx][ny][nz] != 0) {
+					continue;
 				}
 
-				// 2. 벽이지롱
-				if (board[nx][ny] == 1 && broken == 0 && visited[nx][ny][1] == false) {
-					visited[nx][ny][1] = true;
-					queue.add(new int[] {nx, ny, 1});
-					dist[nx][ny][1] = dist[px][py][broken] + 1;
+				// wall?
+				if (map[nx][ny] == 1) {
+					// not broke yet?
+					if (pz == 0) {
+						nz = 1;
+						dist[nx][ny][nz] = dist[px][py][pz] + 1;
+						queue.add(new int[]{nx, ny, nz});
+					}
+
+					// alreay broke?
+					if (pz == 1) {
+						continue;
+					}
+				}
+
+				// not wall?
+				if (map[nx][ny] == 0) {
+					dist[nx][ny][nz] = dist[px][py][pz] + 1;
+					queue.add(new int[]{nx, ny, nz});
 				}
 			}
 		}
+		return -1;
 	}
 }
